c147183449546dc2a7a0580c5f2f862590c664ab
Daniel Kato <dmkato@gmail.com>
2019-10-20 17:05:43 -0700

    got node

--- create_daily_notes.py
+++ create_daily_notes.py
@@ -1,31 +1,9 @@
 #!/usr/bin/env python3
 import re
-from utils.Bear import Bear
-
-
-def tokenize_note(note):
-    return [("##" if idx != 0 else "") + section for (idx, section) in enumerate(note.split("##"))]
-
-
-def remove_completed(section):
-    lines = section.split("\n")
-    completed = get_completed_tasks(lines)
-    # completed = [line for line in lines if re.match(r'\s*\+', line)]
-    incomplete = [line for line in lines if line not in completed]
-    return ("\n".join(incomplete), "\n".join(completed))
-
+from Bear.Bear import Bear
+from ToDo.ToDoList import ToDoList
 
 if __name__ == "__main__":
     bear = Bear()
-    note = bear.open_note(title="ToDo")
-
-    (title, today, tomorrow, this_week,
-        this_month, yesterday) = tokenize_note(note)
-    (new_today, completed) = remove_completed(today)
-    (yesterday_title, _) = remove_completed(yesterday)
-    new_yesterday = yesterday_title + completed
-    new_note = title + new_today + tomorrow + \
-        this_week + this_month + new_yesterday
-
-    print(f'Note: \n{note}')
-    print(f'New Note: \n{new_note}')
+    note_markdown = bear.open_note(title="ToDo")
+    to_do_note = ToDoList(note_markdown)
